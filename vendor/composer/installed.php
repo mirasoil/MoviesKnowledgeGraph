8312c5fcd04126046ae2bc0b1d2cf94080f4b148
mirasoil <mirasoilproduction@gmail.com>
2021-04-27 15:54:24 +0300

    Movie Knowledge Graph - bugs

---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'easyrdf/easyrdf' => 
+    array (
+      'pretty_version' => '1.1.1',
+      'version' => '1.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c7b0a9dbcb211eb7de03ee99ff5b52d17f2a8e64',
+    ),
+    'ml/iri' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cbd44fa913e00ea624241b38cefaa99da8d71341',
+    ),
+    'ml/json-ld' => 
+    array (
+      'pretty_version' => '1.2.0',
+      'version' => '1.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c74a1aed5979ed1cfb1be35a55a305fd30e30b93',
+    ),
+  ),
+);
