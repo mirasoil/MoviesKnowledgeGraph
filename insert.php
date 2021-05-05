e91b903578cac056582f81cd44db4b15c48a742f
mirasoil <mirasoilproduction@gmail.com>
2021-05-05 18:46:25 +0300

    bugs on success

--- insert.php
+++ insert.php
@@ -15,26 +15,20 @@ $mass = $_POST['mass'];
 
 $adresa="http://localhost:8080/rdf4j-server/repositories/robograph?query=";
 
-//actor = on insert - uri, movie = on delete - uri, movieTitle = FE, skills = FE, director = FE, image = FE, releaseDate = FE
 $query = "PREFIX : <http://ispasteodora.ro#> ASK WHERE{ BIND(REPLACE(\"".$uriNou."\",' ','') AS ?numeFaraSpatii) BIND(CONCAT('http://ispasteodora.ro#',?numeFaraSpatii) AS ?uriString) BIND(IRI(?uriString) AS ?idNou) ?idNou ?x ?y}";  
 
 $interogare=urlencode($query);
-// print $interogare;
+
 $clienthttp=new EasyRdf\Http\Client($adresa.$interogare);
 
 $clienthttp->setHeaders("Accept","application/sparql-results+json");
 
 $rezultatJSON=$clienthttp->request()->getBody();
 
-// print $rezultatJSON;
 
 $rezultatRestructurat=new EasyRdf\Sparql\Result($rezultatJSON,"application/sparql-results+json");
-// print $rezultatRestructurat;
+
 if($rezultatRestructurat == "true") {
-    // print 'Acest URI exita deja in baza de cunostinte';
-    // return json_encode(array('statusCode' => 201 , 'error' => 'Acest URI exita deja in baza de cunostinte'));
-    // header("Content-Type: application/json; charset=utf-8", true);
-    // echo json_encode(array('status' => 201 , 'error' => 'exista deja'));
     $response_array = array("error"=> false,
                             "status" => false,
                             "message" => 'Acest URI exita deja in baza de cunostinte');
@@ -57,9 +51,6 @@ if($rezultatRestructurat == "true") {
 
     $rezultat=$client->update($interogare);
 
-    // echo 'Rezultat '.json_encode($rezultat);
-    // header("Content-Type: application/json; charset=utf-8", true);
-    // echo json_encode(array('status' => 200 , 'success' => 'inserat'));
     $response_array = array("success"=> true,
                                 "status" => true,
                                 "message" => 'Inserat cu succes');
